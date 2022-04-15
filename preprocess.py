89392ed716112bb58e9d70ba342461dbb9bb146a
nguyenhungquang <42964528+nguyenhungquang@users.noreply.github.com>
2022-04-16 00:25:56 +0700

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -23,12 +23,9 @@ def remove_dup(text):
     d = {k: v for (k, v) in zip(uniChars, unsignChars)}
     return re.sub(fr'\S([{uniChars}])\1+', replace, text)
 
-def merge_percent(text):
-    return re.sub(r'([0-9]*)\s*phần trăm', r'\1%', text)
 
 def preprocess(text):
     text = remove_dup(text)
-    text = merge_percent(text)
     return text
    
 if __name__ == '__main__':
