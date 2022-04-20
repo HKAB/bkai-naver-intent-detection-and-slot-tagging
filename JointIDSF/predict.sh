19e26044eabcc3508803fead3232046ac8437329
nguyenhungquang <hungquangnguyen66@gmail.com>
2022-04-20 09:46:54 -0700

    add jointidsf

---
+++ JointIDSF/predict.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=1 python3 predict.py --input_file BKAI/syllable-level/dev/seq.in \
+                              --output_file predictions.txt \
+                              --model_dir JointIDSF_XLM-Rencoder/3e-5/0.25/10/
